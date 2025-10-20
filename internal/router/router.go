cf08d5348d52652d432159df9543b2ee8b0fc8ec
Ivan <astanin.i.a@gmail.com>
2025-10-20 21:31:00 +0200

    middleware fix

--- internal/router/router.go
+++ internal/router/router.go
@@ -15,11 +15,9 @@ func NewRouter(databases *db.DB) *gin.Engine {
 
 	r.Use(gin.Recovery())
 	r.Use(middleware.Logging())
-	r.Use(middleware.DeviceMiddleware())
 
 	v1 := r.Group("/api/v1")
 	{
-		// Device routes
 		deviceRepo := repository.NewDeviceRepo(databases)
 		deviceService := service.NewDeviceService(deviceRepo)
 		deviceHandler := handler.NewDeviceHandler(deviceService)
@@ -29,89 +27,93 @@ func NewRouter(databases *db.DB) *gin.Engine {
 			users.GET("/device", deviceHandler.AddDeviceID)
 		}
 
-		// Timecode routes
-		timecodeRepo := repository.NewTimecodeRepo(databases)
-		timecodeService := service.NewTimecodeService(timecodeRepo)
-		timecodeHandler := handler.NewTimecodeHandler(timecodeService)
-
-		timecodes := v1.Group("/timecode")
+		authV1 := v1.Group("/")
+		authV1.Use(middleware.DeviceMiddleware())
 		{
-			timecodes.GET("", timecodeHandler.GetTimecode)
-			timecodes.GET("/all", timecodeHandler.GetAllTimecodes)
-			timecodes.POST("", timecodeHandler.AddOrUpdateTimecode)
-		}
+			// Timecode routes
+			timecodeRepo := repository.NewTimecodeRepo(databases)
+			timecodeService := service.NewTimecodeService(timecodeRepo)
+			timecodeHandler := handler.NewTimecodeHandler(timecodeService)
 
-		// History routes
-		historyRepo := repository.NewHistoryRepo(databases)
-		historyService := service.NewHistoryService(historyRepo)
-		historyHandler := handler.NewHistoryHandler(historyService)
-
-		history := v1.Group("/history")
-		{
-			history.POST("", historyHandler.AddHistory)
-			history.GET("", historyHandler.GetHistory)
-			history.GET("/all", historyHandler.GetAllHistory)
-		}
-
-		// Favourite routes
-		favRepo := repository.NewFavouriteRepo(databases)
-		favService := service.NewFavouriteService(favRepo)
-		favHandler := handler.NewFavouriteHandler(favService)
-
-		favourite := v1.Group("/favourite")
-		{
-			favourite.POST("", favHandler.AddFavourite)
-			favourite.DELETE("", favHandler.RemoveFavourite)
-			favourite.GET("", favHandler.GetFavourites)
-			favourite.GET("/all", favHandler.GetAllFavourites)
-		}
-
-		// Collection routes
-		collectionRepo := repository.NewCollectionRepo(databases)
-		collectionService := service.NewCollectionService(collectionRepo)
-		collectionHandler := handler.NewCollectionHandler(collectionService)
-
-		collection := v1.Group("/collection")
-		{
-			collection.POST("", collectionHandler.AddCollection)
-			collection.DELETE("", collectionHandler.RemoveCollection)
-			collection.GET("", collectionHandler.GetCollections)
-			collection.GET("/all", collectionHandler.GetAllCollections)
-		}
-
-		// Anime routes
-		animeRepo := repository.NewAnimeRepo(databases)
-		animeService := service.NewAnimeService(animeRepo)
-		animeHandler := handler.NewAnimeHandler(animeService)
-
-		anime := v1.Group("/anime")
-		{
-			// Consumet routes
-			consumet := anime.Group("/consumet")
+			timecodes := authV1.Group("/timecode")
 			{
-				consumet.GET("/", animeHandler.SearchConsumetAnime)
-				consumet.GET("/genres", animeHandler.GetConsumetGenres)
-				consumet.GET("/latest", animeHandler.SearchConsumetLatestReleases)
-				consumet.GET("/recommended", animeHandler.SearchConsumetRecommendedAnime)
-				consumet.GET("/genre/releases", animeHandler.SearchConsumetGenreReleases)
-				consumet.GET("/:id", animeHandler.GetAnimeInfoByConsumetID)
-				consumet.GET("/episode/:id", animeHandler.GetConsumetEpisodeInfo)
+				timecodes.GET("", timecodeHandler.GetTimecode)
+				timecodes.GET("/all", timecodeHandler.GetAllTimecodes)
+				timecodes.POST("", timecodeHandler.AddOrUpdateTimecode)
 			}
 
-			// Anilibria routes
-			anilibria := anime.Group("/anilibria")
+			// History routes
+			historyRepo := repository.NewHistoryRepo(databases)
+			historyService := service.NewHistoryService(historyRepo)
+			historyHandler := handler.NewHistoryHandler(historyService)
+
+			history := authV1.Group("/history")
 			{
-				anilibria.GET("/", animeHandler.SearchAnilibriaAnime)
-				anilibria.GET("/genres", animeHandler.GetAnilibriaGenres)
-				anilibria.GET("/latest", animeHandler.SearchAnilibriaLatestReleases)
-				anilibria.GET("/random", animeHandler.SearchAnilibriaRandomReleases)
-				anilibria.GET("/recommended", animeHandler.SearchAnilibriaRecommendedAnime)
-				anilibria.GET("/genre/releases", animeHandler.SearchAnilibriaGenreReleases)
-				anilibria.GET("/:id", animeHandler.GetAnimeInfoByAnilibriaID)
-				anilibria.GET("/episode/:id", animeHandler.GetAnilibriaEpisodeInfo)
+				history.POST("", historyHandler.AddHistory)
+				history.GET("", historyHandler.GetHistory)
+				history.GET("/all", historyHandler.GetAllHistory)
 			}
 
-			anime.GET("/search/:id", animeHandler.SearchAnimeByID)
+			// Favourite routes
+			favRepo := repository.NewFavouriteRepo(databases)
+			favService := service.NewFavouriteService(favRepo)
+			favHandler := handler.NewFavouriteHandler(favService)
+
+			favourite := authV1.Group("/favourite")
+			{
+				favourite.POST("", favHandler.AddFavourite)
+				favourite.DELETE("", favHandler.RemoveFavourite)
+				favourite.GET("", favHandler.GetFavourites)
+				favourite.GET("/all", favHandler.GetAllFavourites)
+			}
+
+			// Collection routes
+			collectionRepo := repository.NewCollectionRepo(databases)
+			collectionService := service.NewCollectionService(collectionRepo)
+			collectionHandler := handler.NewCollectionHandler(collectionService)
+
+			collection := authV1.Group("/collection")
+			{
+				collection.POST("", collectionHandler.AddCollection)
+				collection.DELETE("", collectionHandler.RemoveCollection)
+				collection.GET("", collectionHandler.GetCollections)
+				collection.GET("/all", collectionHandler.GetAllCollections)
+			}
+
+			// Anime routes
+			animeRepo := repository.NewAnimeRepo(databases)
+			animeService := service.NewAnimeService(animeRepo)
+			animeHandler := handler.NewAnimeHandler(animeService)
+
+			anime := authV1.Group("/anime")
+			{
+				// Consumet routes
+				consumet := anime.Group("/consumet")
+				{
+					consumet.GET("/", animeHandler.SearchConsumetAnime)
+					consumet.GET("/genres", animeHandler.GetConsumetGenres)
+					consumet.GET("/latest", animeHandler.SearchConsumetLatestReleases)
+					consumet.GET("/recommended", animeHandler.SearchConsumetRecommendedAnime)
+					consumet.GET("/genre/releases", animeHandler.SearchConsumetGenreReleases)
+					consumet.GET("/:id", animeHandler.GetAnimeInfoByConsumetID)
+					consumet.GET("/episode/:id", animeHandler.GetConsumetEpisodeInfo)
+				}
+
+				// Anilibria routes
+				anilibria := anime.Group("/anilibria")
+				{
+					anilibria.GET("/", animeHandler.SearchAnilibriaAnime)
+					anilibria.GET("/genres", animeHandler.GetAnilibriaGenres)
+					anilibria.GET("/latest", animeHandler.SearchAnilibriaLatestReleases)
+					anilibria.GET("/random", animeHandler.SearchAnilibriaRandomReleases)
+					anilibria.GET("/recommended", animeHandler.SearchAnilibriaRecommendedAnime)
+					anilibria.GET("/genre/releases", animeHandler.SearchAnilibriaGenreReleases)
+					anilibria.GET("/:id", animeHandler.GetAnimeInfoByAnilibriaID)
+					anilibria.GET("/episode/:id", animeHandler.GetAnilibriaEpisodeInfo)
+				}
+
+				anime.GET("/search/:id", animeHandler.SearchAnimeByID)
+			}
 		}
 	}
 
